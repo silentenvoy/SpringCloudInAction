6deddf00af294e3b1c94856257bc34c085c1f843
cisnotcpp <cisnotcpp@outlook.com>
2021-12-25 18:09:33 +0800

    添加权限访问的

--- organizationservice/src/main/java/wiki/cccp/organizationservice/OrganizationserviceApplication.java
+++ organizationservice/src/main/java/wiki/cccp/organizationservice/OrganizationserviceApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "wiki.cccp.organizationservice.mapper")
 @EnableDiscoveryClient
+@EnableResourceServer
 public class OrganizationserviceApplication {
 
     public static void main(String[] args) {
