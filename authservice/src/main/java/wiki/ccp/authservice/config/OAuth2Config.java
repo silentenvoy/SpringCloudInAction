0d3aafeb6cc809e4fc122df9b54d65d76aa11a83
cisnotcpp <cisnotcpp@outlook.com>
2021-12-26 22:07:35 +0800

    fix up OAuth2Config

--- authservice/src/main/java/wiki/ccp/authservice/config/OAuth2Config.java
+++ authservice/src/main/java/wiki/ccp/authservice/config/OAuth2Config.java
@@ -1,37 +1,22 @@
 package wiki.ccp.authservice.config;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class OAuth2Config extends OAuth2AuthorizationServerConfiguration {
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
     private UserDetailsService userDetailsService;
-
-    public OAuth2Config(BaseClientDetails details, AuthenticationConfiguration authenticationConfiguration,
-                        ObjectProvider<TokenStore> tokenStore, ObjectProvider<AccessTokenConverter> tokenConverter,
-                        AuthorizationServerProperties properties) throws Exception {
-        super(details, authenticationConfiguration, tokenStore, tokenConverter, properties);
-    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
